28419a2cd1fc2c7dec301f2904a58955090af80a
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-11-09 16:04:36 -0500

    Created the post path for lists and a new endpoint if the list doesn't save

--- app.rb
+++ app.rb
@@ -9,3 +9,12 @@ get "/lists" do
   @lists = List.all
   erb :"lists/index.html", layout: :"layout/application.html"
 end
+
+post "/lists" do
+  @list = List.new(params)
+  if @list.save
+    redirect "/lists"
+  else
+    erb :"lists/new.html", layout: :"layout/application.html"
+  end
+end
